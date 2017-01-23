88a64bfc69ba1061eef4277153451635cde600b3
Federico Baldassarre <baldassarre.fe@gmail.com>
2017-01-23 10:55:43 +0100

    Delta and Sepdata
    Implemented delta.m with fitting visualization, added some missing stuff
    in sepdata.m

---
+++ delta.m
@@ -0,0 +1,27 @@
+clear;
+[patterns, targets] = sepdata;
+
+% Initialization
+eta = 0.001;
+X = [patterns; ones(1, size(patterns, 2))];
+w = randn(1, size(X, 1));
+epochs = 20;
+
+for i = 0:epochs
+    deltaW = -eta*( w*X - targets)*X';
+    w = w + deltaW;
+        
+    % Show
+    p = w(1, 1:2);
+    k = -w(1, size(patterns, 1)+1) / (p*p');
+    l = sqrt(p*p');
+    axis([-2, 2, -2, 2], 'square');
+    plot (patterns(1, find(targets>0)), ...
+        patterns(2, find(targets>0)), '*', ...
+        patterns(1, find(targets<0)), ...
+        patterns(2, find(targets<0)), '+', ...
+        [p(1), p(1)]*k + [-p(2), p(2)]/l, ...
+        [p(2), p(2)]*k + [p(1), -p(1)]/l, '-');
+    drawnow;
+    pause(0.5);
+end
--- sepdata.m
+++ sepdata.m
@@ -1,4 +1,4 @@
-function [ classA, classB ] = sepdata(  )
+function [ patterns, targets ] = sepdata(  )
     A_mean = [1.0, 0.5];
     A_std = 0.5;
     B_mean = [-1.0,0.5];
@@ -7,7 +7,11 @@ function [ classA, classB ] = sepdata(  )
     classA(2,:) = randn(1,100) .* A_std + A_mean(2);
     classB(1,:) = randn(1,100) .* B_std + B_mean(1);
     classB(2,:) = randn(1,100) .* B_std + B_mean(2);
-
-
+    
+    patterns = [classA, classB];
+    targets = [ones(1,100), - ones(1,100)];
+    permute = randperm(200);
+    patterns = patterns(:, permute);
+    targets = targets(:, permute);
 end
 
