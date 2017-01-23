a642a2a4206d7c4a765c2504b3feebdfc9bc0587
Federico Baldassarre <baldassarre.fe@gmail.com>
2017-01-23 11:00:25 +0100

    Non separable data
    Added the non separable dataset and fixed the plot limits in delta.m

--- delta.m
+++ delta.m
@@ -1,5 +1,10 @@
 clear;
-[patterns, targets] = sepdata;
+
+% Easily separable dataset
+%[patterns, targets] = sepdata;
+
+% Non separable dataset
+[patterns, targets] = nsepdata;
 
 % Initialization
 eta = 0.001;
@@ -15,13 +20,13 @@ for i = 0:epochs
     p = w(1, 1:2);
     k = -w(1, size(patterns, 1)+1) / (p*p');
     l = sqrt(p*p');
-    axis([-2, 2, -2, 2], 'square');
     plot (patterns(1, find(targets>0)), ...
         patterns(2, find(targets>0)), '*', ...
         patterns(1, find(targets<0)), ...
         patterns(2, find(targets<0)), '+', ...
         [p(1), p(1)]*k + [-p(2), p(2)]/l, ...
         [p(2), p(2)]*k + [p(1), -p(1)]/l, '-');
+    axis([-2, 2, -2, 2], 'square');
     drawnow;
     pause(0.5);
 end
