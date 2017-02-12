dc02bca3fbbbe767299267d02f5b8b19ece43551
LilKdl <lucasrg@kth.se>
2017-02-12 23:29:04 +0100

    added some documentation

--- lab3/neighborhood2.m
+++ lab3/neighborhood2.m
@@ -1,11 +1,16 @@
 function [ k_neighbors ] = neighborhood2(k, radius, grid_side) 
-%NEIGHBORHOOD2 Function to get the linearized index of the neighbours as a column vector
-%   Input the linearized index of the winner and the threshold.
+%NEIGHBORHOOD2 Function to get the linearized index of the neighbours as a 
+% column vector input the linearized index of the winner and the threshold.
+%
+% * k: unit to find neighbours of
+% * radius: radius of neighbourhood
+% * grid_size: size of the grid, in the lab example it is 10x10
+%
     [x, y] = meshgrid(1:grid_side, 1:grid_side);
     is = reshape(x, 1, grid_side^2);
     js = reshape(y, 1, grid_side^2);
     i = is(k);
     j = js(k);
     dist = abs(x-i) + abs(y-j);
-    k_neighbors = reshape(dist - radius, grid_side^2, 1) <= 0;
+    k_neighbors = reshape(dist, grid_side^2, 1) <= radius;
 end
