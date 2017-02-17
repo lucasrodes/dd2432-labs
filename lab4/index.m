e3ac23a6c66d4ae3603f08bdf30b0eff3eee3111
Federico Baldassarre <baldassarre.fe@gmail.com>
2017-02-17 09:45:22 +0100

    Lab 4 initial commit

---
+++ lab4/index.m
@@ -0,0 +1,12 @@
+%% Artificial Neural Network and Other Learning Systems - Lab 4
+
+%%
+% <html>
+% <ul>
+% <li><a href="eight_neurons.html">Introduction and small sample</a></li>
+% <li><a href="image_seq.html">32x32 image and sequential update</a></li>
+% <li><a href="energy.html">Energy formulation</a></li>
+% <li><a href="distortion_capacity.html">Distortion and capacity</a></li>
+% <li><a href="sparse_patterns.html">Sparse patterns</a></li>
+% </ul>
+% </html>
