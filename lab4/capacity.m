1b295f1b549def6705a0cf561b1103e35dc238c0
Federico Baldassarre <baldassarre.fe@gmail.com>
2017-02-18 11:56:44 +0100

    Finally some interesting explanation for the network capacity

--- lab4/capacity.m
+++ lab4/capacity.m
@@ -6,21 +6,38 @@ clc; clear; close all;
 
 %% Our procedure
 %
-% * Take a network of N=1024 neurons
-% * Train the network on P patterns
-% * For every training pattern p:
-%   * Consider 10 distorted versions of it, each one with noise_quantity pixels flipped
-%   * Check how many of the distorted patterns get correctly reconstructed
-% * Express the performance of the network as #correctly reconstructed/
-% #patterns
+% <html>
+% <ol>
+% <li>Take a network of N neurons</li>
+% <li>Train the network on P patterns</li>
+% <li>For every training pattern p:
+% <ol>
+%   <li>Consider several distorted versions of it, each one with 
+%   noise_quantity pixels flipped</li>
+%   <li>Check how many of the distorted patterns get correctly reconstructed
+%   (allow for a few wrong pixels)</li>
+% </ol></li>
+% <li>Express the denoising performance of the network as #correctly
+% reconstructed / #attempted patterns</li>
+% <li>Express the stability of the network as #stable patterns / #patterns</li>
+% </ol>
+% </html>
+%
 
-
-%% Test 1
-pict;
+%% 9 image patterns
+% Not working very well after 4 patterns, due to the imbalance between +1
+% and -1 pixels in the images.
+clear;  pict;
 all_patterns = [p1; p2; p3; p4; p5; p6; p7; p8; p9];
-N = 1024;
+print_pattern_stat(all_patterns);
 noisy_pixels = 100;
-repetitions = 50;
+repetitions = 20;
+
+figure;
+for i=1:9
+    subplot(3,3,i);
+    vis(all_patterns(i, :));
+end
 
 rng(1);
 performances = [];
@@ -33,7 +50,7 @@ for P = 1:size(all_patterns, 1)
         for i=1:repetitions
             distorted_pat = flip_img(original_pat', noisy_pixels)';
             reconstructed_pat = evolve_net(w, distorted_pat);
-            if sum(original_pat~=reconstructed_pat)<10
+            if sum(original_pat~=reconstructed_pat)<5
                 successes = successes + 1;
             end
         end
@@ -45,9 +62,17 @@ end
 
 figure;
 plot(1:size(all_patterns, 1), performances);
+ylim([0, 1]);
+grid on;
+title('Denoising of the memories');
+xlabel('Number of training patterns');
+ylabel('Percent of denoised patterns'); 
 
-%% Test 2
-rng(1);
+suptitle('9 image patterns');
+%% 25 randomly generated rectangular patterns
+% Same issues as before, due to the imbalance between +1
+% and -1 pixels in the images.
+clear; rng(1);
 all_patterns = ones(25, 1024);
 figure;
 for i = 1:25
@@ -69,10 +94,10 @@ for i = 1:25
     axis off;
 end
 
-noisy_pixels = 20;
+print_pattern_stat(all_patterns);
+noisy_pixels = 100;
 repetitions = 10;
 
-rng(1);
 performances = [];
 for P = 2:size(all_patterns, 1)
     patterns = all_patterns(1:P, :);
@@ -96,13 +121,25 @@ end
 figure;
 plot(2:size(all_patterns, 1), performances);
 ylim([0, 1]);
+grid on;
+title('Denoising of the memories');
+xlabel('Number of training patterns');
+ylabel('Percent of denoised patterns'); 
 
-%% Test 3
-rng(1);
+suptitle('25 randomly generated rectangular patterns');
+%% 290 zero centered random patterns
+% Now instead of images, we train the network on many patterns that are
+% randomly generated from a zero centered distribution. This means that the
+% probability of a pixel being 1 or -1 in any of the training patterns is
+% 0.5.
+%
+% With zero centered patterns the performances of the network are actually
+% similar to the empirical 0.138N limit.
+clear; rng(1);
 all_patterns = sgn(randn(290, 1024));
-N = 1024;
-noisy_pixels = 20;
-repetitions = 5;
+print_pattern_stat(all_patterns);
+noisy_pixels = 100;
+repetitions = 3;
 
 performances = [];
 for P = 70:15:size(all_patterns, 1)
@@ -127,16 +164,20 @@ end
 figure;
 plot(70:15:size(all_patterns, 1), performances);
 grid on;
-title('Network performance');
+title('Denoising of the memories');
 xlabel('Number of training patterns');
-ylabel('Percent of reconstructed patterns');
+ylabel('Percent of denoised patterns'); 
 
-%% Test 4
-rng(1);
+suptitle('290 zero centered random patterns');
+%% 300 zero centered random patterns
+% Same thing happens with a smaller network (note that the number of noisy 
+% pixels and the similarity threshold have been lowered accordingly).
+clear; rng(1);
 all_patterns = sgn(randn(300, 100));
+print_pattern_stat(all_patterns);
 N = 100;
-noisy_pixels = 20;
-repetitions = 2;
+noisy_pixels = 30;
+repetitions = 3;
 
 performances = [];
 stables = [];
@@ -157,7 +198,7 @@ for P = 1:size(all_patterns, 1)
         for i=1:repetitions
             distorted_pat = flip_img(original_pat', noisy_pixels)';
             reconstructed_pat = evolve_net(w, distorted_pat);
-            if sum(original_pat~=reconstructed_pat)<5
+            if sum(original_pat~=reconstructed_pat)<3
                 successes = successes + 1;
             end
         end
@@ -173,13 +214,137 @@ figure;
 subplot(1,2,1);
 plot(1:size(all_patterns, 1), performances);
 grid on;
-title('Network performance');
+title('Denoising of the memories');
 xlabel('Number of training patterns');
-ylabel('Percent of reconstructed patterns');
+ylabel('Percent of denoised patterns'); 
 
 subplot(1,2,2);
 plot(1:size(all_patterns, 1), stables);
 grid on;
-title('Network performance');
+title('Stability of the memories');
 xlabel('Number of training patterns');
 ylabel('Percent of training patterns that are stable');
+
+suptitle('300 zero centered random patterns');
+%% 300 zero centered random patterns + diagonal suppression
+% With the diagonal suppressed the network is much less prone to remain
+% stable in the configuration it is. 
+%
+% This can be seen if we compare the current stability plot with the one above: 
+%
+% *now after the breaking point no memorized pattern is stable
+% * previously many patterns remained stable after the breaking point, 
+% but just because the network was less likely to change configuration, 
+% not because the pattern was a meaningful energy minimum.
+%
+clear; rng(1);
+all_patterns = sgn(randn(300, 100));
+print_pattern_stat(all_patterns);
+noisy_pixels = 30;
+repetitions = 3;
+
+performances = [];
+stables = [];
+for P = 1:size(all_patterns, 1)
+    patterns = all_patterns(1:P, :);
+    w = train_weights(patterns, true);
+    
+    successes = 0;
+    stable_patterns = 0;
+    for original_pat = patterns'
+        % try if one original pattern remains stable after one iter
+        after_one_iter = sgn(w * original_pat);
+        if isequal(after_one_iter, original_pat)
+            stable_patterns = stable_patterns + 1;
+        end
+        
+        % try if a distorted pattern remains stable after one iter
+        for i=1:repetitions
+            distorted_pat = flip_img(original_pat', noisy_pixels)';
+            reconstructed_pat = evolve_net(w, distorted_pat);
+            if sum(original_pat~=reconstructed_pat)<3
+                successes = successes + 1;
+            end
+        end
+    end
+    
+    performance = successes / (P * repetitions);
+    performances = [performances, performance];
+    stables = [stables, stable_patterns/P];
+end
+
+figure;
+
+subplot(1,2,1);
+plot(1:size(all_patterns, 1), performances);
+grid on;
+title('Denoising of the memories');
+xlabel('Number of training patterns');
+ylabel('Percent of denoised patterns'); 
+
+subplot(1,2,2);
+plot(1:size(all_patterns, 1), stables);
+grid on;
+title('Stability of the memories');
+xlabel('Number of training patterns');
+ylabel('Percent of training patterns that are stable');
+
+suptitle('300 zero centered random patterns + diagonal suppression');
+%% 300 NON zero centered random patterns + diagonal suppression
+% It is worse than the previous in denoising and stability, because the
+% patterns are not zero centered.
+clear; rng(1);
+all_patterns = sgn(0.5 + randn(300, 100));
+print_pattern_stat(all_patterns);
+noisy_pixels = 30;
+repetitions = 3;
+
+performances = [];
+stables = [];
+for P = 1:size(all_patterns, 1)
+    patterns = all_patterns(1:P, :);
+    w = train_weights(patterns, true);
+    
+    successes = 0;
+    stable_patterns = 0;
+    for original_pat = patterns'
+        % try if one original pattern remains stable after one iter
+        after_one_iter = sgn(w * original_pat);
+        if isequal(after_one_iter, original_pat)
+            stable_patterns = stable_patterns + 1;
+        end
+        
+        % try if a distorted pattern remains stable after one iter
+        for i=1:repetitions
+            distorted_pat = flip_img(original_pat', noisy_pixels)';
+            reconstructed_pat = evolve_net(w, distorted_pat);
+            if sum(original_pat~=reconstructed_pat)<3
+                successes = successes + 1;
+            end
+        end
+    end
+    
+    performance = successes / (P * repetitions);
+    performances = [performances, performance];
+    stables = [stables, stable_patterns/P];
+end
+
+figure;
+
+subplot(1,2,1);
+plot(1:size(all_patterns, 1), performances);
+grid on;
+title('Denoising of the memories');
+xlabel('Number of training patterns');
+ylabel('Percent of denoised patterns'); 
+
+subplot(1,2,2);
+plot(1:size(all_patterns, 1), stables);
+grid on;
+title('Stability of the memories');
+xlabel('Number of training patterns');
+ylabel('Percent of training patterns that are stable');
+
+suptitle('300 NON zero centered random patterns + diagonal suppression');
+%%
+close all;
--- lab4/train_weights.m
+++ lab4/train_weights.m
@@ -1,10 +1,12 @@
-function [ w ] = train_weights(patterns)
+function [ w ] = train_weights(patterns, suppress_diagonal)
 % patterns is a PxN matrix, where P is the number of patterns and N is the
 % dimensionality of each pattern. The functions computes the NxN weight 
 % matrix of a Hopfield network with N neurons.
     [P, N] = size(patterns);
     w = patterns' * patterns ./ N;
-    return
+    if nargin>1 && suppress_diagonal
+        w = w - diag(diag(w));
+    end
 end
 
 % Tip: create 3 patterns of 8 bits like this:
