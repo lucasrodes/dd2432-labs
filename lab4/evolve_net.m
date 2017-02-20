5a9581b78ff95017ed87588f988cdf3222894ca8
Diego González Morín <Diego Gonzlez Morn>
2017-02-20 15:51:21 +0100

    Sparse patterns experiments

--- lab4/evolve_net.m
+++ lab4/evolve_net.m
@@ -3,10 +3,18 @@
 % sequential: Activate it if asynchronous update is wanted
 % bias: Activate it to activate the bias term.
 
-function [ x_end, iterations ] = evolve_net(w, x_start, sequential, bias)
+% x_start and x_end are column vectors
+function [ x_end, iterations ] = evolve_net(w, x_start, sequential, bias, conv_seek,  bias_val)
+
     N = size(w, 1);
     iterations = 0;
     
+    if nargin < 6
+       bias_val = sum(x_start)/length(x_start);
+    end
+    if nargin < 5
+       conv_seek = true;
+    end
     if nargin < 4
         bias = false;
     end
@@ -43,19 +51,21 @@ function [ x_end, iterations ] = evolve_net(w, x_start, sequential, bias)
                     converged = false;
                 end
             end
+            if conv_seek == false
+                return;
+            end
         end
         subplot(1,2,1);
         vis(x_end);
         title(sprintf('Converged iter: %d', ions));
     else
         converged = false;
-        while not(converged) && iterations < 100
+        while not(converged) && iterations < 100 
             if bias == false
                 x_end = sgn(w * x_start);
             else
                 %Bias term is chosen to be the mean of patterns 
-                biais = sum(x_start)/length(x_start);
-                x_end = 0.5 + 0.5 * sgn( (x_start'*w' - biais) );
+                x_end = 0.5 + 0.5 * sgn( (x_start'*w' - bias_val) );
             end
             converged = isequal(x_start, x_end);
             iterations = iterations + 1;
@@ -64,7 +74,9 @@ function [ x_end, iterations ] = evolve_net(w, x_start, sequential, bias)
             else
                 x_start = x_end;
             end
+            if conv_seek == false
+                return;
+            end
         end
     end
-end
 
--- lab4/sparse_patterns.m
+++ lab4/sparse_patterns.m
@@ -6,9 +6,9 @@ clc; clear; close all;
 
 %% First test - study the maximum saving capacity of the network using the bias
 
-clc;clear;close all;
+clc;clear;
 
-N = 100;
+N = 200;
 P = 100;
 biais = 0.0;
 % Create P patterns with a biais 
@@ -29,7 +29,7 @@ for P = 1:size(all_patterns, 1)
     %w = w-diag(diag(w));
     saved = 0;
     for original_pat = patterns'
-        reconstructed_pat_Bias = evolve_net(w_Bias, original_pat,false,true);
+        reconstructed_pat_Bias = evolve_net(w_Bias, original_pat,false,true,false);
         if sum(abs(original_pat'-reconstructed_pat_Bias)) == 0
             saved = saved + 1;
         end
@@ -39,6 +39,133 @@ for P = 1:size(all_patterns, 1)
 end
 
 plot(0:pat-1, percentage_vec, 'b+-');
+
+%% First experiment: 
+% 10% activity
+
+clc;clear;
+
+N = 200;
+P = 100;
+
+act_perc = 0.1;
+
+%lets play with the value of the bias
+bias = 0;
+
+% Create P patterns with only a 10% of activation
+all_patterns = zeros(P, N);
+
+%Generate a image with a certain percentage of activation
+for i=1:P
+    index = randperm(N);
+    index = index(1:round(N*act_perc));
+    all_patterns(i,index) = 1;
+end
+
+%We need to compute the mean of all_patterns
+[pat, N] = size(all_patterns);
+m = sum(sum(all_patterns))/(N*pat);
+
+rng(1);
+
+%
+Max_bias_val = 15;
+step_val = 3;
+
+count = 0;
+
+figure;
+for bias=0:step_val:Max_bias_val
+    percentage_vec =[];
+    for P = 1:size(all_patterns, 1)
+        patterns = all_patterns(1:P,:);
+        w_Bias = train_weights(patterns - m,false,true);
+        % Transform w gto resist to noise
+        %w = w-diag(diag(w));
+        saved = 0;
+        for original_pat = patterns'
+            reconstructed_pat_Bias = evolve_net(w_Bias, original_pat,false,true,false,bias);
+            if sum(abs(original_pat'-reconstructed_pat_Bias)) == 0
+                saved = saved + 1;
+            end
+        end
+        % Add percentage of good pattern stored
+        percentage_vec = [percentage_vec saved*100/P];
+    end
+    hold all
+    plot(0:pat-1, percentage_vec,'LineWidth',2)
+    count = count + 1;
+    legendInfo{count} = ['bias = ' num2str(bias)];
+end
+legend( legendInfo)
+hold off
+
+%% Second experiment
+%The objective now is to study the performance when the activity percentage
+%is even smaller
+
+clc;clear;
+
+N = 200;
+P = 100;
+
+rng(1);
+
+%Bias
+Max_bias_val = 15;
+step_val = 3;
+
+%Act percentage
+initial_val = 0.01;
+step_act = 0.01;
+final_act_val = 0.05
+
+
+
+for bias = 0:step_val:Max_bias_val
+    figure;
+    count = 0;
+    all_patterns = zeros(P, N);
+    for act_perc = initial_val:step_act:final_act_val
+        %Generate a image with a certain percentage of activation
+        for i=1:P
+            index = randperm(N);
+            index = index(1:round(N*act_perc));
+            all_patterns(i,index) = 1;
+        end
+
+        %We need to compute the mean of all_patterns
+        [pat, N] = size(all_patterns);
+        m = sum(sum(all_patterns))/(N*pat);
+
+
+        percentage_vec =[];
+        for P = 1:size(all_patterns, 1)
+            patterns = all_patterns(1:P,:);
+            w_Bias = train_weights(patterns - m,false,true);
+            % Transform w gto resist to noise
+            %w = w-diag(diag(w));
+            saved = 0;
+            for original_pat = patterns'
+                reconstructed_pat_Bias = evolve_net(w_Bias, original_pat,false,true,false,bias);
+                if sum(abs(original_pat'-reconstructed_pat_Bias)) == 0
+                    saved = saved + 1;
+                end
+            end
+            % Add percentage of good pattern stored
+            percentage_vec = [percentage_vec saved*100/P];
+        end
+        hold all
+        plot(0:pat-1, percentage_vec,'LineWidth',2)
+        count = count + 1;
+        legendInfo{count} = ['Activation Percentage = ' num2str(act_perc)];
+    end
+    legend( legendInfo)
+    legendInfo = [];
+    title(sprintf('Bias = %d',bias));
+    hold off
+end
 
 %%
 close all;
