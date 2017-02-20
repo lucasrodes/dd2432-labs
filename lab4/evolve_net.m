777a42182051b11e635927a5396b5369650678cb
LilKdl <lucasrg@kth.se>
2017-02-20 16:55:42 +0100

    merge evolve

--- lab4/evolve_net.m
+++ lab4/evolve_net.m
@@ -3,15 +3,20 @@
 % sequential: Activate it if asynchronous update is wanted
 % bias: Activate it to activate the bias term.
 
-function [ x_end, iterations ] = evolve_net(w, x_start, sequential, bias)
+function [ x_end, iterations ] = evolve_net(w, x_start, patterns, ...
+    sequential, bias)
+
     N = size(w, 1);
     iterations = 0;
     
-    if nargin < 4
+    if nargin < 5
         bias = false;
     end
-    if nargin < 3
+    if nargin < 4
         sequential = false;
+    end
+    if nargin < 3
+        patterns = 0;
     end
     if nargin < 2
         x_start = sgn(randn(N, 1));
@@ -20,33 +25,60 @@ function [ x_end, iterations ] = evolve_net(w, x_start, sequential, bias)
     if sequential
         converged = false;
         x_end = x_start;
-        figure;
+        figure(1);
+        E = zeros(1, 10000);
         while not(converged) && iterations < 10000
-            converged = true;
+%             converged = true;
             for i = randperm(N)
-                old_x = x_end(i);
+%                 old_x = x_end(i);
                 x_end(i) = sgn(w(i,:) * x_end);
                 iterations = iterations + 1;
+                E(iterations) = - x_end' * w * x_end;
                 if mod(iterations, 10)==0
                     subplot(1,2,1);
                     vis(x_end);
-                    title(sprintf('Iter: %d', iterations));
+                    title(sprintf('Iter: %d', iterations),...
+                        'Interpreter', 'latex', 'Fontsize', 16);
                     subplot(1,2,2);
                     hold on;
-                    E = - x_end' * w * x_end;
-                    plot(iterations, E, '.b');
-                    xlabel('Iterations');
-                    ylabel('Energy');
+                    
+                    plot(iterations, E(iterations), '.b');
+                    title('Network Energy',...
+                        'Interpreter', 'latex', 'Fontsize', 16);
+                    xlabel('Iterations', 'Interpreter', 'latex',...
+                        'Fontsize', 16);
+                    ylabel('Energy', 'Interpreter', 'latex',...
+                        'Fontsize', 16);
                     pause(0.05);
                 end
-                if old_x ~= x_end(i)
-                    converged = false;
+                % Convergence criteria
+                if iterations>1
+                    s = find(diff(E(1:iterations))~=0);
+                    if isempty(s)
+                        s = iterations
+                    end
+                    constant_energy = iterations - s(end);
+                    [ismem, id] = ismember(x_end', patterns, 'rows');
+                    if ismem
+                        converged = true;
+                        h = sprintf('Match found with pattern p%d at iteration %d', id, iterations);
+                        break;
+                    elseif constant_energy>N/4
+                        h = sprintf('Converged, but no match found! at iteration %d', iterations);;
+                        converged = true;
+                        break;
+                    else
+                        converged = false;
+                    end
                 end
+%                 if old_x ~= x_end(i)
+%                     converged = false;
+%                 end
             end
         end
         subplot(1,2,1);
         vis(x_end);
-        title(sprintf('Converged iter: %d', ions));
+        title(h, 'Interpreter', 'latex', 'Fontsize', 16);
     else
         converged = false;
         while not(converged) && iterations < 100
